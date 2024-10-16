ae11e3d2c49d0f50c57418fd07a4484180bddc8d
Pavan Sukalkar <pavan.s@kukufm.com>
2024-10-16 23:11:41 +0530

    Added Installation Guide

---
+++ install.sh
@@ -0,0 +1,18 @@
+# Install docker
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+# Install git
+sudo yum install git -y
+
+# Install pip
+sudo yum install python-pip -y
+
+# Make venv
+python3 -m venv db-test-env
+
+# Install packages
+# cd pvs-db-testing
+# pip install -r requirements.txt
--- redis_manager/base.py
+++ redis_manager/base.py
@@ -1,18 +1,28 @@
 import redis
 
 class RedisManager():
+
+    def __init__(self) -> None:
+        self.client = RedisManager.get_client()
+
     @staticmethod
     def get_client():
         pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
         client = redis.Redis(connection_pool=pool)
         return client
 
-    def add_data(data):
-        client = RedisManager.get_client()
-        client.set(data)
+    def add_data(self, data):
+        for k, v in data.items():
+            self.client.set(k, v)
 
-    def get_data():
-        client = RedisManager.get_client()
-        keys = client.keys('*')
+    def get_data(self):
+        keys = self.client.keys('*')
         for k in keys:
             print(k)
+
+    def add_bulk_data(self):
+        emps = list()
+        for i in range(1000):
+            emps.append({f'emp{i}': f'Employee {i}'})
+        for emp in emps:
+            self.add_data(emp)
