f2b7fc902e4378318c7946a6acd619d576ce036b
Pavan Sukalkar <pavan.s@kukufm.com>
2024-10-18 22:30:13 +0530

    Added subscription

--- tic_tac_toe/board.py
+++ tic_tac_toe/board.py
@@ -4,6 +4,7 @@ from redis_manager import RedisManager
 class Board():
     def __init__(self) -> None:
         self.pubsub = RedisManager.get_pubsub()
+        self.pubsub.subscribe('tic-tac-toe')
         self.board = self.prepare_board()
 
     def prepare_board(self):
@@ -17,9 +18,9 @@ class Board():
 
     def print_board(self):
         for row in self.board:
-            row_view = f""
+            row_view = f"|"
             for m in row:
-                row_view = f"|{row_view}|"
+                row_view = f"{row_view}|{m}|"
             print(row_view)
             print("-"*9)
     
