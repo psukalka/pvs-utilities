1273ac6bc4bf27692efef36adc580e5d9f22bdf3
Pavan Sukalkar <pavan.s@kukufm.com>
2024-10-22 18:53:04 +0530

    Using Cursor AI to generate code

--- tic_tac_toe/board.py
+++ tic_tac_toe/board.py
@@ -1,4 +1,5 @@
 import json
+import threading
 from redis_manager import RedisManager
 
 # Consumer
@@ -10,23 +11,41 @@ class Board():
 
     def prepare_board(self):
         matrix = list()
-        for i in range(3):
-            row = []
-            for j in range(3):
-                row.append(-1)
+        for _ in range(3):
+            row = [-1] * 3
             matrix.append(row)
         return matrix
 
     def print_board(self):
         for row in self.board:
-            row_view = f"|"
-            for m in row:
-                row_view = f"{row_view}{m}|"
+            row_view = "|" + "|".join(str(m) for m in row) + "|"
             print(row_view)
-            print("-"*9)
+            print("-" * 9)
     
-    def get_move(self):
-        message = self.pubsub.get_message()
-        if message:
-            message = json.loads(message)
-            print(message)
+    def update_board(self, message):
+        player = message['player']
+        marker = message['marker']
+        x = message['x']
+        y = message['y']
+        
+        if self.board[y][x] == -1:
+            self.board[y][x] = marker
+            print(f"Player {player} placed {marker} at position ({x}, {y})")
+            self.print_board()
+        else:
+            print(f"Invalid move: Position ({x}, {y}) is already occupied")
+
+    def handle_message(self, message):
+        data = message['data'].decode('utf-8')
+        if data:
+            move = json.loads(data)
+            self.update_board(move)
+
+    def start_listening(self):
+        def listen():
+            for message in self.pubsub.listen():
+                if message['type'] == 'message':
+                    self.handle_message(message)
+
+        thread = threading.Thread(target=listen, daemon=True)
+        thread.start()
