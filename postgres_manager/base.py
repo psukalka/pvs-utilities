6f509ab53533cceb8a76974ed87704c81256f520
Pavan Sukalkar <pavan.s@kukufm.com>
2024-11-16 19:55:08 +0530

    Added pg manager

--- postgres_manager/base.py
+++ postgres_manager/base.py
@@ -1,2 +1,21 @@
+import psycopg2
+
 class PostgresManager:
-    pass
+    def __init__(self) -> None:
+        self.client = PostgresManager.get_client()
+
+    @staticmethod
+    def get_client():
+        client = psycopg2.connect(
+            host="localhost",
+            database="postgres",
+            user="postgres",
+            password="pvs123"
+        )
+        return client
+
+    def get_data(self):
+        cursor = self.client.cursor()
+        cursor.execute("SELECT * FROM employees")
+        return cursor.fetchall()
+
