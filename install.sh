f00eeea90e24855c19a415455edfdcd943ae3673
Pavan Sukalkar <pavan.s@kukufm.com>
2024-11-20 18:42:31 +0530

    minor changes

--- install.sh
+++ install.sh
@@ -10,6 +10,12 @@ sudo yum install git -y
 # Install pip
 sudo yum install python-pip -y
 
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
 # Make venv
 python3 -m venv db-test-env
 
