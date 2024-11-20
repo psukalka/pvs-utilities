ac49ef5c780318bc23688369c6d116b7adb638c0
Pavan Sukalkar <pavan.s@kukufm.com>
2024-11-20 17:19:33 +0530

    data backup

--- postgres_manager/install_pg.sh
+++ postgres_manager/install_pg.sh
@@ -1,8 +1,11 @@
 # Getting latest postgres image
 docker pull postgres:latest
 
+# Create volume to store data
+docker volume create my_db_data
+
 # Running postgres container    
-docker run -d --name pg_test -e POSTGRES_PASSWORD=pvs123 -p 5432:5432 postgres:latest
+docker run -d --name pg_test -v my_db_data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=pvs123 -p 5432:5432 postgres:latest
 
 # Second docker
-docker run -d --name pg_backup -e POSTGRES_PASSWORD=pvs123 -p 5433:5433 postgres:latest
+docker run -d --name pg_backup -v my_db_data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=pvs123 -p 5433:5433 postgres:latest
