949a99a0ae0b956abc593abb71e2b482a1fa9ce3
Pavan Sukalkar <pavan.s@kukufm.com>
2024-11-21 09:09:02 +0530

    minor fixes

--- install.sh
+++ install.sh
@@ -16,6 +16,11 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 
+# Keyfile is needed for using authentication with replicaset
+# Generate a keyfile with appropriate permissions
+openssl rand -base64 756 > mongo-keyfile
+chmod 400 mongo-keyfile
+
 # Make venv
 python3 -m venv db-test-env
 
