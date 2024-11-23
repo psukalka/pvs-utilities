135b65020b12ed6605ba4ec093a4d041aa6a0a4c
Pavan Sukalkar <pavan.s@kukufm.com>
2024-11-23 20:34:10 +0530

    Final configs

--- mongo_manager/mongo_shard.sh
+++ mongo_manager/mongo_shard.sh
@@ -1,13 +1,12 @@
 # Connect to the container
-docker exec -it s1n1 mongosh -u admin -p pvs123
+docker exec -it s1n1 mongosh --port 27018
 
-# Inside mongosh, initialize the replica set
 rs.initiate({
   _id: "rs0",
   members: [
-    {_id: 0, host: "s1n1:27017"},
-    {_id: 1, host: "s1n2:27017"},
-    {_id: 2, host: "s1n3:27017"}
+    {_id: 0, host: "s1n1:27018"},
+    {_id: 1, host: "s1n2:27018"},
+    {_id: 2, host: "s1n3:27018"}
   ]
 })
 
@@ -16,16 +15,15 @@ rs.status()
 
 ########
 
-docker exec -it s2n1 mongosh -u admin -p pvs123
+docker exec -it s2n1 mongosh --port 27018
 
-# Inside mongosh of s2n1
 rs.initiate({
-    _id: "rs1",
-    members: [
-        {_id: 0, host: "s2n1:27017"},
-        {_id: 1, host: "s2n2:27017"},
-        {_id: 2, host: "s2n3:27017"}
-    ]
+  _id: "rs1",
+  members: [
+    {_id: 0, host: "s2n1:27018"},
+    {_id: 1, host: "s2n2:27018"},
+    {_id: 2, host: "s2n3:27018"}
+  ]
 })
 
 rs.status()
@@ -46,6 +44,53 @@ rs.initiate({
 #######
 docker exec -it mongo_router mongosh
 
-sh.addShard("rs0/s1n1:27017,s1n2:27017,s1n3:27017")
-sh.addShard("rs1/s2n1:27017,s2n2:27017,s2n3:27017")
+sh.addShard("rs0/s1n1:27018,s1n2:27018,s1n3:27018")
+sh.addShard("rs1/s2n1:27018,s2n2:27018,s2n3:27018")
 
+sh.enableSharding("test")
+
+use test
+
+db.createCollection("users")
+db.users.createIndex({ "user_id": "hashed" })
+sh.shardCollection("test.users", { "user_id": "hashed" })
+
+
+db.users.insertOne({
+    user_id: 1,
+    name: "Pavan Sukalkar",
+    email: "pvsukalkar@gmail.com"
+})
+
+db.users.insertMany([
+    {
+        user_id: 2,
+        name: "Sonali Ghogare",
+        email: "sona@gmail.com"
+    },
+    {
+        user_id: 3,
+        name: "Shivam Sukalkar",
+        email: "shivam@gmail.com"
+    }
+])
+
+db.users.insertMany([
+    {
+        user_id: 4,
+        name: "John Doe",
+        email: "john@example.com"
+    },
+    {
+        user_id: 5,
+        name: "Jane Doe",
+        email: "jane@example.com"
+    },
+    {
+        user_id: 6,
+        name: "Bob Smith",
+        email: "bob@example.com"
+    }
+])
+
+db.users.getShardDistribution()
