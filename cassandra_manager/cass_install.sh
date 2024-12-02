9679963d053fd0d7a24975df386ab4fc31440f6f
Pavan Sukalkar <pavan.s@kukufm.com>
2024-12-02 21:39:34 +0530

    Added cassandra docker file

--- cassandra_manager/cass_install.sh
+++ cassandra_manager/cass_install.sh
@@ -11,7 +11,7 @@ java -version
 sudo bash -c 'cat << EOF > /etc/yum.repos.d/cassandra.repo
 [cassandra]
 name=Apache Cassandra
-baseurl=https://downloads.apache.org/cassandra/redhat/41x/
+baseurl=https://redhat.cassandra.apache.org/41x/
 gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://downloads.apache.org/cassandra/KEYS
@@ -19,9 +19,17 @@ EOF'
 
 sudo yum install cassandra -y
 
-sudo systemctl start cassandra
 sudo systemctl enable cassandra
+sudo systemctl start cassandra
 
 sudo systemctl status cassandra
 nodetool status
 
+# sudo vim /etc/cassandra/conf/cassandra.yaml
+# Make sure these settings are correct:
+
+# listen_address: Should be set to your EC2 instance's private IP
+# rpc_address: Set to 0.0.0.0
+# broadcast_rpc_address: Should be set to your EC2 instance's private IP
+# seeds: Should include your instance's private IP
+# Make sure endpoint_snitch is set to SimpleSnitch for a single-node setup
