03b1112d6231d28a48ba4dc9f225462ea6278c08
Pavan Sukalkar <pavan.s@kukufm.com>
2024-12-09 19:01:57 +0530

    added serializer deserializer

--- managers/memcached/base.py
+++ managers/memcached/base.py
@@ -1,8 +1,10 @@
+import json
 from pymemcache.client import base
 
 class MemcachedManager:
     def __init__(self, host='localhost', port=11211):
-        self.client = base.Client((host, port))
+        self.client = base.Client((host, port), serializer=self.json_serializer,
+                                  deserializer=self.json_deserializer)
 
     def set(self, key, value, expire=0):
         """Store data in Memcached."""
@@ -11,6 +13,18 @@ class MemcachedManager:
     def get(self, key):
         """Retrieve data from Memcached."""
         return self.client.get(key)
+    
+    def json_serializer(self, key, value):
+        if type(value) == str:
+            return value, 1
+        return json.dumps(value), 2
+    
+    def json_deserializer(self, value, flag):
+        if flag == 1:
+            return value.decode('utf-8')
+        elif flag == 2:
+            return json.loads(value.decode('utf-8'))
+        raise Exception(f"Unknown Serialization format")
 
 # Example usage:
 # manager = MemcachedManager()
