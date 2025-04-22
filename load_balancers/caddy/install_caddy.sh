d5e9040ff15ee0a068c9b2cc093692ea09c21bf0
Pavan Sukalkar <pavan.s@kukufm.com>
2025-04-23 00:18:30 +0530

    updated script

--- load_balancers/caddy/install_caddy.sh
+++ load_balancers/caddy/install_caddy.sh
@@ -1,8 +1,4 @@
-# First, let's install the required dependencies
-sudo yum install -y yum-utils
-
-# Add the official Caddy repo
-sudo yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/g/caddy/caddy/repo/epel-7/group_caddy-caddy-epel-7.repo
-
-# Now install Caddy
-sudo yum install -y caddy
+# Install caddy 
+dnf install 'dnf-command(copr)'
+dnf copr enable @caddy/caddy
+dnf install caddy
